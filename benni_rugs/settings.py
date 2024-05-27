cbfecf9478fcf9ae33147f91da305ca84537d0b5
Behnam Pordavam <behnampordavam@gmail.com>
2024-05-27 14:19:41 +0000

    Remove secret key and det debug

--- benni_rugs/settings.py
+++ benni_rugs/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j8bpgqima^lz%q^ye6-u3lj8ok&8+zj-*jdc)i)md2bic)wn$-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['benni-rugs-78e83a64387b.herokuapp.com', 'localhost']
 
